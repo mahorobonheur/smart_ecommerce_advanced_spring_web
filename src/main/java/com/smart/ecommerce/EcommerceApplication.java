7b529be345f4694dd4463dbb970b492063aacfbb
mahorobonheur <mahorobonheur@gmail.com>
2026-02-05 14:52:26 +0200

    Add mongoDB new Database settings

--- src/main/java/com/smart/ecommerce/EcommerceApplication.java
+++ src/main/java/com/smart/ecommerce/EcommerceApplication.java
@@ -1,13 +1,18 @@
 package com.smart.ecommerce;
 
+import com.smart.ecommerce.repository.ReviewRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EntityScan("com.smart.ecommerce.model")
+@EnableMongoRepositories(
+        basePackageClasses = ReviewRepository.class
+)
 @EnableCaching
 public class EcommerceApplication {
 
